572e9b74589a0b69d75bf56f7b24dbc30babcf1f
XingHai Lu <xinghai.lu@grabtaxi.com>
2021-06-17 10:34:14 +0800

    update .travis.yml add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='moniter_bus',
+      version='0.0.1',
+      description='Get peking bus station and position',
+      author='dev-J-Ariza',
+      author_email='',
+      url='',
+      packages=find_packages(),
+     )
